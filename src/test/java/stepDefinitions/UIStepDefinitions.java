ea7facc25a471b0942fc6f19572f81ce34ba7199
asjadt <119504845+asjadt@users.noreply.github.com>
2025-03-09 05:24:48 +0000

    Update UIStepDefinitions.java
    Added ChromeDriver Location

--- src/test/java/stepDefinitions/UIStepDefinitions.java
+++ src/test/java/stepDefinitions/UIStepDefinitions.java
@@ -13,7 +13,17 @@ public class UIStepDefinitions {
 
     @Given("I open the browser and navigate to {string}")
     public void i_open_the_browser_and_navigate_to(String url) {
-        System.setProperty("webdriver.chrome.driver", "/Users/atariq/Downloads/chromedriver-mac-x64/chromedriver");
+
+         String os = System.getProperty("os.name").toLowerCase();
+    if (os.contains("win")) {
+        // Path for Windows
+        System.setProperty("webdriver.chrome.driver", "C:\Program Files\chromedriver\chromedriver.exe");
+    } else if (os.contains("mac")) {
+        // Path for Mac
+        System.setProperty("webdriver.chrome.driver", "/Users/username/Downloads/chromedriver-mac-x64/chromedriver");
+    } else {
+        throw new RuntimeException("Unsupported operating system: " + os);
+    }
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--no-sandbox");
